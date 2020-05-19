555630415586a75d86428ec73162ee887a269409
zephinzer <1324745-zephinzer@users.noreply.gitlab.com>
2020-05-20 02:30:21 +0800

    updated constants (removed useless platforms)

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -68,8 +68,6 @@ var (
 	OpenAliases                 = []string{"op", "o"}
 	PivotalTrackerCanonicalNoun = "pivotaltracker"
 	PivotalTrackerAliases       = []string{"pivotal", "pt"}
-	PlatformCanonicalNoun       = "platform"
-	PlatformAliases             = []string{"platforms", "pf"}
 	RepositoryCanonicalNoun     = "repository"
 	RepositoryAliases           = []string{"repositories", "repo", "repos", "rp", "r"}
 	ServerCanonicalNoun         = "server"
