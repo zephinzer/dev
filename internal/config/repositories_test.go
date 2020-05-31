2dc79f0299ba6f95c56567133fcc75f271605575
zephinzer <1324745-zephinzer@users.noreply.gitlab.com>
2020-05-31 12:09:55 +0800

    fixed flaky tests due to array ordering

--- internal/config/repositories_test.go
+++ internal/config/repositories_test.go
@@ -17,43 +17,46 @@ func TestRepositories(t *testing.T) {
 func (s RepositoriesTests) TestGetWorkspaces() {
 	repos := Repositories{
 		{
-			CloneURL:   "git@github.com:zephinzer/dev.git",
+			URL:        "git@github.com:zephinzer/dev.git",
 			Name:       "repo 1",
 			Workspaces: []string{"ab", "cd"},
 		},
 		{
-			CloneURL:   "git@github.com:zephinzer/dev.git",
+			URL:        "git@github.com:zephinzer/dev.git",
 			Name:       "repo 2",
 			Workspaces: []string{"abc", "def"},
 		},
 		{
-			CloneURL:   "git@github.com:zephinzer/dev.git",
+			URL:        "git@github.com:zephinzer/dev.git",
 			Name:       "repo 3",
 			Workspaces: []string{"ab", "def"},
 		},
 	}
-	s.EqualValues([]string{"ab", "cd", "abc", "def"}, repos.GetWorkspaces())
+	s.Contains(repos.GetWorkspaces(), "ab")
+	s.Contains(repos.GetWorkspaces(), "cd")
+	s.Contains(repos.GetWorkspaces(), "abc")
+	s.Contains(repos.GetWorkspaces(), "def")
 }
 
 func (s RepositoriesTests) TestMergeWith() {
 	reposA := Repositories{
 		{
-			Name:     "repo1",
-			CloneURL: "git@github.com:user1/repo1.git",
+			Name: "repo1",
+			URL:  "git@github.com:user1/repo1.git",
 		},
 		{
-			Name:     "repo2",
-			CloneURL: "git@github.com:user1/repo2.git",
+			Name: "repo2",
+			URL:  "git@github.com:user1/repo2.git",
 		},
 	}
 	reposB := Repositories{
 		{
-			Name:     "repo1",
-			CloneURL: "git@github.com:user2/repo1.git",
+			Name: "repo1",
+			URL:  "git@github.com:user2/repo1.git",
 		},
 		{
-			Name:     "repo2",
-			CloneURL: "git@github.com:user2/repo2.git",
+			Name: "repo2",
+			URL:  "git@github.com:user2/repo2.git",
 		},
 	}
 	s.Len(reposA, 2)
