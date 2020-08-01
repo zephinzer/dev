23a2e36d2a4bc9ce0b6e2b2b650edc82dc09f8b0
zephinzer <1324745-zephinzer@users.noreply.gitlab.com>
2020-08-01 17:39:10 +0800

    added dev.client.platforms.github tests

--- internal/config/dev_test.go
+++ internal/config/dev_test.go
@@ -23,4 +23,7 @@ func (s *DevConfigTests) TestMarshal() {
 	s.Equal("__expected_database_path", c.Dev.Client.Database.Path)
 	s.Equal("__expected_telegram_token", c.Dev.Client.Notifications.Telegram.Token)
 	s.Equal("__expected_telegram_chat_id", c.Dev.Client.Notifications.Telegram.ID)
+	s.Equal("__expected_platform_github_client_id", c.Dev.Client.Platforms.Github.ClientID)
+	s.Equal("__expected_platform_github_client_secret", c.Dev.Client.Platforms.Github.ClientSecret)
+	s.Equal("__expected_platform_github_redirect_uri", c.Dev.Client.Platforms.Github.RedirectURI)
 }
